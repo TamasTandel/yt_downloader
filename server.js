3d54e86181700bcf7f62cc1ab73631883ee6b1cc
TamasTandel <tamas310t@gmail.com>
2025-05-22 15:16:28 +0530

    origin:*

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const BASE_URL = process.env.BASE_URL;
 
 app.use(cors({
-    origin: ['https://yt-download-fron.vercel.app']
+    origin: '*'
 }));
 app.use(express.json());
 
